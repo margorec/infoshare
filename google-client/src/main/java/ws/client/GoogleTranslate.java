9c40bee53e3be85f9ae401ff7e2f51896a771015
Marcin Górecki <marcin@mbp-marcin.budapest.epam.com>
2017-05-09 15:38:32 +0200

    Fixed unittest

--- google-client/src/main/java/ws/client/GoogleTranslate.java
+++ google-client/src/main/java/ws/client/GoogleTranslate.java
@@ -1,8 +1,14 @@
 package ws.client;
 
+import ws.client.model.Data;
+import ws.client.model.Output;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class GoogleTranslate {
@@ -14,8 +20,26 @@ public class GoogleTranslate {
     }
 
     public String translationFor(String input) {
-       return null;
+        Client client = ClientBuilder.newClient();
+
+        Form paramsForm = new Form();
+        paramsForm.param("source", "en");
+        paramsForm.param("target", "pl");
+        paramsForm.param("key", API_KEY);
+        paramsForm.param("q", input);
+
+        WebTarget target = client.target("https://translation.googleapis.com/language/translate/v2");
+
+        Response response = target.request().accept(MediaType.APPLICATION_JSON).post(Entity.form(paramsForm));
+        Output data = response.readEntity(Output.class);
+
+        response.close();
+
+        return data.getData().getTranslations().get(0).getTranslatedText();
     }
 
 
+
 }
+
+
--- google-client/src/test/java/ws/client/GoogleTranslateTest.java
+++ google-client/src/test/java/ws/client/GoogleTranslateTest.java
@@ -1,13 +1,21 @@
 package ws.client;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
+import ws.client.model.Data;
+import ws.client.model.Output;
+import ws.client.model.Translation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GoogleTranslateTest {
 
-    final String key = "";
+    final String key = "AIzaSyBde9PPInnRvdcwiMzie1AQLvdxdVZdvoY";
 
     @Test
     public void should_translate_with_google() {
@@ -16,6 +24,26 @@ public class GoogleTranslateTest {
 
         GoogleTranslate t = new GoogleTranslate(key);
         assertThat(t.translationFor(input), equalTo(output));
+    }
+
+    @Test
+    public void aaa() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Output output = new Output();
+        Data d = new Data();
+
+
+        List<Translation> tl = new ArrayList<Translation>();
+        Translation t = new Translation();
+        t.setTranslatedText("Dsdsds");
+
+        tl.add(t);
+        d.setTranslations(tl);
+        output.setData(d);
+
+        System.out.println(mapper.writeValueAsString(output));
+
     }
 
 }
